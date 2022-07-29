658bde1d9c6f3bf54636dd96e4fe38bb0794d25a
wood <amper.sil.tok@gmail.com>
2022-07-29 12:40:14 +0300

    added to the DB schema - cascade deletion

--- db.sql
+++ db.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS clients_groups
 username varchar unique not null,
 client_group varchar not null,
 
-CONSTRAINT FK_clients_groups_clients FOREIGN KEY (username) REFERENCES clients(username)
+CONSTRAINT FK_clients_groups_clients FOREIGN KEY (username) REFERENCES clients(username) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS clients_friends
@@ -19,5 +19,5 @@ CREATE TABLE IF NOT EXISTS clients_friends
 username varchar unique not null,
 client_friend varchar not null,
 
-CONSTRAINT FK_clients_friends_clients FOREIGN KEY (username) REFERENCES clients(username)
+CONSTRAINT FK_clients_friends_clients FOREIGN KEY (username) REFERENCES clients(username) ON DELETE CASCADE
 );
