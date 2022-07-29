1bba6e3ccbab2bd4836c9a73370313d020388d49
wood <amper.sil.tok@gmail.com>
2022-07-29 10:13:12 +0300

    refactoring db schema

--- db.sql
+++ db.sql
@@ -3,7 +3,21 @@ CREATE TABLE IF NOT EXISTS clients
 client_id  serial PRIMARY KEY,
 username varchar unique NOT null,
 passwd varchar NOT null,
-registration_date int not null,
-friend_list text[],
-room_list text[]
+registration_date int not null
+);
+
+CREATE TABLE IF NOT EXISTS clients_groups
+(
+username varchar unique not null,
+client_group varchar not null,
+
+CONSTRAINT FK_clients_groups_clients FOREIGN KEY (username) REFERENCES clients(username)
+);
+
+CREATE TABLE IF NOT EXISTS clients_friends
+(
+username varchar unique not null,
+client_friend varchar not null,
+
+CONSTRAINT FK_clients_friends_clients FOREIGN KEY (username) REFERENCES clients(username)
 );
